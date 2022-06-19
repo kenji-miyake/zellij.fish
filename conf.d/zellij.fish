5ea008ffc352a513051bb4bcc1ca1509b24bf13f
Kenji Miyake <kenji.miyake@tier4.jp>
2022-06-19 13:44:53 +0900

    Remove abbr
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- conf.d/zellij.fish
+++ conf.d/zellij.fish
@@ -1,7 +1,6 @@
 set -q ZELLIJ_AUTO_START || set -U ZELLIJ_AUTO_START true
 set -q ZELLIJ_AUTO_EXIT || set -U ZELLIJ_AUTO_EXIT true
 
-abbr -a z zellij
 zellij setup --generate-completion fish | source
 
 if type zellij-auto-start >/dev/null 2>&1
