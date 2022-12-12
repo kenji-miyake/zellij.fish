8f6e8711577f8475a43d183dbb0e692bb51d5997
Kenji Miyake <kenji.miyake@tier4.jp>
2022-12-12 15:03:09 +0900

    fix: add ZELLIJ_AUTO_START
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- functions/zellij-auto-start.fish
+++ functions/zellij-auto-start.fish
@@ -5,9 +5,12 @@ function zellij-auto-start
     set current_number (zellij list-sessions | rg "automatic-(\w+)" -or '$1' | sort -r | head -n 1)
     test -z "$current_number" && set current_number 0
     set new_number (math "$current_number" + 1)
-    zellij -s "automatic-$new_number"
 
-    if $ZELLIJ_AUTO_EXIT
-        kill $fish_pid
+    if $ZELLIJ_AUTO_START
+        zellij -s "automatic-$new_number"
+
+        if $ZELLIJ_AUTO_EXIT
+            kill $fish_pid
+        end
     end
 end
