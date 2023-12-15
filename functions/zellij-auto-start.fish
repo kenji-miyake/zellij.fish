ad4422852e321a6008b23709fbb911d1fbe04b9a
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2023-12-15 11:20:49 +0900

    fix: use -V for sort

--- functions/zellij-auto-start.fish
+++ functions/zellij-auto-start.fish
@@ -2,7 +2,7 @@ function zellij-auto-start
     set -q ZELLIJ && return
     set -q VSCODE_GIT_IPC_HANDLE && return
 
-    set current_number (zellij list-sessions | rg "automatic-(\w+)" -or '$1' | sort -r | head -n 1)
+    set current_number (zellij list-sessions | rg "automatic-(\w+)" -or '$1' | sort -rV | head -n 1)
     test -z "$current_number" && set current_number 0
     set new_number (math "$current_number" + 1)
 
